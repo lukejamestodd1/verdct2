08978eccf1b880bb5c328a51470a5af347797c04
Luke Todd <lukejamestodd1@gmail.com>
2016-06-20 20:05:28 +1000

    h

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ var savedEventsController = require('./routes/api/savedEventsController');
 //app
 var app = express();
 
-// uncomment after placing favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/mainController.js
+++ routes/mainController.js
@@ -19,7 +19,7 @@ router.post('/register', function(req, res) {
     	email : req.body.email 
     }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            return res.render('register', { account : account, title: 'register' });
         }
         passport.authenticate('local')(req, res, function () {
             res.redirect('/home');
