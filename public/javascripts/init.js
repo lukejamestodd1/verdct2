e6d05746f33b42267d7ef022c8771cfd18e586ac
Luke Todd <lukejamestodd1@gmail.com>
2016-07-04 03:11:37 +1000

    event add

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
-    $('.parallax').parallax();
-     $('.modal-trigger').leanModal();
-      $('#textarea1').val('');
-  $('#textarea1').trigger('autoresize');
+    $('#textarea1').val('');
+  	$('#textarea1').trigger('autoresize');
+  	$('select').material_select();
+  	$('.collapsible').collapsible();
+  	$('.carousel').carousel({ padding: 10});
+  	$('.datepicker').pickadate({
+    	selectMonths: true, // Creates a dropdown to control month
+    	selectYears: 15 // Creates a dropdown of 15 years to control year
+  	});
 });
