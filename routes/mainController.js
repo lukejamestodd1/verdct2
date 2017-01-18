c0a8ba6b9c32d5a28ca28e4919a17eebda762333
Luke Todd <lukejamestodd1@gmail.com>
2017-01-18 21:02:44 +1100

    hdhfh

--- routes/mainController.js
+++ routes/mainController.js
@@ -39,7 +39,7 @@ router.get('/newevent', function(req, res, next) {
 });
 
 router.get('/newdress', function(req, res, next) {
-  res.render('newdress', { title: 'Create Event', user: req.user});
+  res.render('newdress', { title: 'New Dress', user: req.user});
 });
 
 router.get('/events', function(req, res, next) {
